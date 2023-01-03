c70d27b57bb706cd1b1131db63295418a1a596d9
r-tomiyama <peko858.ab@gmail.com>
2023-01-03 20:31:55 +0900

    add: useSWR

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Box } from '@chakra-ui/react';
 import { Room } from './parts';
 import { useRooms } from '../../hooks/firestore/useRooms';
-import { Room as RoomType } from '../../services/firestore/types/Room';
 
 export const RoomList: React.FC = () => {
-  const [rooms, setRooms] = useState<RoomType[]>();
-  useEffect(() => {
-    const fetchRooms = async () => {
-      const { rooms } = await useRooms();
-      setRooms(rooms);
-    };
-    void fetchRooms();
-  });
-
-  return <Box>{rooms && rooms.map((r, i) => <Room key={i} name={r.name} />)}</Box>;
+  const { rooms, isValidating } = useRooms();
+  return (
+    <>
+      {isValidating || (
+        <Box>
+          {rooms.map((r, i) => (
+            <Room key={i} name={r.name} />
+          ))}
+        </Box>
+      )}
+    </>
+  );
 };
--- src/hooks/firestore/useRooms.ts
+++ src/hooks/firestore/useRooms.ts
@@ -1,11 +1,39 @@
 import { getDocs } from 'firebase/firestore';
+import useSWR from 'swr';
+
 import { roomsCollection } from '../../services/firestore/firestoreService';
+import { Room } from '../../services/firestore/types/Room';
 
-export const useRooms = async () => {
+const fetcher = async () => {
   const querySnapshot = await getDocs(roomsCollection);
-  const res = querySnapshot.docs.map((d) => d.data());
+  const rooms = querySnapshot.docs.map((d) => d.data());
 
-  return {
-    rooms: res,
-  };
+  return { rooms };
+};
+
+type ReturnType =
+  | {
+      rooms: Room[];
+      isValidating: false;
+    }
+  | {
+      rooms: never[];
+      isValidating: true;
+    };
+
+export const useRooms = (): ReturnType => {
+  const { data, isValidating } = useSWR('rooms', fetcher);
+
+  const result: ReturnType =
+    data && !isValidating
+      ? {
+          rooms: data.rooms,
+          isValidating: false,
+        }
+      : {
+          rooms: [],
+          isValidating: true,
+        };
+
+  return result;
 };
